1a6d1012c870932730da3bbf931bbed7253d956b
Kim Bøndergaard <kim@fam-boendergaard.dk>
2015-11-23 18:10:48 +0100

    CanonShell: Make class tick to make class self contained (almost)

--- CannonShell.cpp
+++ CannonShell.cpp
@@ -10,29 +10,76 @@
 #include <stdlib.h>
 #include "Trigonometry.h"
 #include "Logger.h"
+#include "CannonPosEvent.h"
+#include "Viewer.h"
+#include <algorithm> // For std::remove_if
 
 namespace RobotGame {
 
-CannonShell::CannonShell(posx_t posX, posy_t posY,
+unsigned int CannonShell::nextId = 0;
+std::vector<CannonShell> CannonShell::shells;
+
+void CannonShell::FireShell(RobCtrl* owner,
+					posx_t posX, posy_t posY,
+					angle_t direction, range_t range)
+{
+	shells.push_back(CannonShell(owner, posX, posY, direction, range));
+}
+
+CannonShell::CannonShell(RobCtrl* owner, posx_t posX, posy_t posY,
 		angle_t direction, range_t range)
-: prevX(posX), prevY(posY), startTick(Scheduler::getScheduler().getTick()),
+: owner(owner), prevX(posX), prevY(posY),
+  startTick(Scheduler::getScheduler().getTick()),
   currX(posX), currY(posY),
   direction(direction), range(range),
-  blast(false)
+  blast(false), id(++nextId)
 {
-
 }
 
 CannonShell::~CannonShell() {
 	// TODO Auto-generated destructor stub
 }
 
-bool CannonShell::tick(unsigned int tick) {
+
+void CannonShell::tick(unsigned int tick) {
+	std::vector<CannonShell>::iterator shellIt;
+
+	for (shellIt = shells.begin(); shellIt != shells.end(); ++shellIt) {
+		(*shellIt)._tick(tick);
+		if ((*shellIt).blasted()) {
+			RobCtrl* robCtrl = Scheduler::iterateRobots(0);
+			while (robCtrl != 0) {
+				robCtrl->shotBlasted((*shellIt).owner, (*shellIt).getX(), (*shellIt).getY());
+				robCtrl = Scheduler::iterateRobots(robCtrl);
+			}
+
+		}
+		Viewer::PostEvent(
+				new CannonPosEvent(
+						(*shellIt).id,
+						(*shellIt).prevX, (*shellIt).prevY,
+						(*shellIt).currX, (*shellIt).currY,
+						(*shellIt).blast));
+	}
+
+	// TODO: Clean up all blasted shells
+	shells.erase(
+			std::remove_if(
+					shells.begin(),
+					shells.end(),
+					[] (CannonShell& shell) { return shell.blasted();}
+			),
+			shells.end()
+	);
+}
+
+bool CannonShell::_tick(unsigned int tick) {
 
 	range_t tickDistance = static_cast<range_t>(SHELL_SPEED);
 	unsigned int distance = std::min(range, tickDistance);
-
-	Trigonometry::move(currX, currY, direction, distance, &currX, &currY);
+	prevX = currX;
+	prevY = currY;
+	Trigonometry::move(prevX, prevY, direction, distance, &currX, &currY);
 	range -= distance;
 	if (range == 0) {
 		Logger::Log(std::string("@¤¤@¤¤@"),
--- CannonShell.h
+++ CannonShell.h
@@ -14,19 +14,30 @@
 namespace RobotGame {
 
 class CannonShell {
+public:
+	static void FireShell(RobCtrl* owner,
+						posx_t posX, posy_t posY,
+						angle_t direction, range_t range);
+
+	static void tick(unsigned int tick);
+
 private:
 	CannonShell() {};
-public:
-	CannonShell(posx_t posX, posy_t posY,
+	CannonShell(RobCtrl* owner, posx_t posX, posy_t posY,
 			angle_t direction, range_t range);
+public:
 	virtual ~CannonShell();
 
-	bool tick(unsigned int tick); /* Returns true if still in air */
+	bool _tick(unsigned int tick); /* Returns true if still in air */
 	bool blasted() const {return blast;};
 	posx_t getX() const {return currX;};
 	posy_t getY() const {return currY;};
 
 private:
+	static std::vector<CannonShell> shells;
+	static unsigned int nextId;
+
+	RobCtrl* owner;
 	posx_t prevX;
 	posy_t prevY;
 	unsigned int startTick;
@@ -35,6 +46,7 @@ private:
 	angle_t direction;
 	range_t range;
 	bool blast;
+	unsigned int id;
 };
 
 } /* namespace RobotGame */
