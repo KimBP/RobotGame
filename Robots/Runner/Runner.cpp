d87e48fd556d737ee631e7e763657f3f2d00a768
Kim Bøndergaard <kim@fam-boendergaard.dk>
2015-12-30 22:12:52 +0100

    Runner: Change random direction while being hit

--- Robots/Runner/Runner.cpp
+++ Robots/Runner/Runner.cpp
@@ -7,6 +7,7 @@
 
 #include "Runner.h"
 #include "Logger.h"
+#include <cstdlib>
 
 extern "C" RobotGame::Robot* getRobot(RobotGame::RobCtrl* robCtrl)
 {
@@ -20,6 +21,7 @@ const std::string Runner::myName("Runner");
 Runner::Runner(RobotGame::RobCtrl* robCtrl)
 : RobotGame::Robot(robCtrl)
 {
+	srand (time(NULL));
 }
 
 Runner::~Runner() {
@@ -38,15 +40,16 @@ void Runner::run()
 	RobotGame::angle_t direction;
 	RobotGame::armor_t lastArmor = getArmor();
 
-	bool firstTime = true;
+	bool doEscape = true;
 
 	while (1) {
 		if (lastArmor > getArmor()) {
+			doEscape = true;
 			RobotGame::Logger::Log(this, std::string("Have been hurt ")+
 										 std::to_string(lastArmor) +
 										 std::string(" -> ")+
 										 std::to_string(getArmor()));
-
+			lastArmor = getArmor();
 		}
 
 		RobotGame::Logger::Log(this, std::string("Current position(")+
@@ -58,32 +61,36 @@ void Runner::run()
 									 std::to_string(getSpeed()) +
 									 std::string(" direction: ") +
 									 std::to_string(getDirection()));
-		if (firstTime) {
-			if (getX() < RobotGame::MAX_POS_X / 2) {
-				horz = GO_E;
-			} else {
-				horz = GO_W;
+		if (doEscape) {
+			int choice;
+			choice = rand() % 3;
+			switch (choice) {
+			case 0: horz = GO_E; break;
+			case 1: horz = GO_W; break;
+			case 2: horz = STAY_H; break;
 			}
-			if (getY() < RobotGame::MAX_POS_Y / 2) {
-				vert = GO_S;
-			} else {
-				vert = GO_N;
+			choice = rand() % 3;
+			switch (choice) {
+			case 0: vert = GO_N; break;
+			case 1: vert = GO_S; break;
+			case 2: vert = STAY_V; break;
 			}
-			firstTime = false;
+
+			doEscape = false;
 		} else {
 			if (getX() < 1000) {
 				horz = GO_E;
 			} else if (getX() > RobotGame::MAX_POS_X - 1000) {
 				horz = GO_W;
 			} else {
-				horz = STAY_H;
+			//	horz = STAY_H;
 			}
 			if (getY() < 1000) {
 				vert = GO_S;
 			} else if (getY() > RobotGame::MAX_POS_Y - 1000) {
 				vert = GO_N;
 			} else {
-				vert = STAY_V;
+			//	vert = STAY_V;
 			}
 		}
 
